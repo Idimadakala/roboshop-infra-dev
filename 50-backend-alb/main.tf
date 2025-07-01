e8d6d584849acaff527c1a1c8769e26b7b16db2f
Sai Srinivas Idimadakala <saisrinivas.idimadakala@gmail.com>
2025-07-02 02:11:16 +0530

    updated backend alb

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -6,7 +6,7 @@ module "alb" {
   vpc_id  = local.vpc_id
   subnets = local.roboshop_private_subnet_ids
   create_security_group = false
-  security_groups = local.backend_alb_sg_id
+  security_groups = [local.backend_alb_sg_id]
   tags = merge(local.common_tags,{
     Name = "${var.project}-${var.environment}-backend-alb"
 
