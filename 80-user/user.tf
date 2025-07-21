84e69423115f8ee03fb5fc3304e52cbde2112af7
Sai Srinivas Idimadakala <saisrinivas.idimadakala@gmail.com>
2025-07-21 09:38:07 +0530

    added component user

--- 80-user/user.tf
+++ 80-user/user.tf
@@ -1,5 +1,5 @@
 module "user" {
     source = "git::https://github.com/Idimadakala/roboshop-infra-component-module.git?ref=develop"
     component = "user"
-    rule_priority = 20
+    rule_priority = 20 # Priority for the user service and 10 for catalouge service
 }
